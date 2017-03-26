3dd76792fdcf3c0a65a78ffff641c737a7f27a56
DaveNow <543260104@qq.com>
2017-03-26 13:06:45 +0800

    Modified templates to display posts from database.

---
+++ blog/migrations/0004_auto_20170326_1255.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_auto_20170325_1604'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='post',
+            old_name='publish_date',
+            new_name='published_date',
+        ),
+    ]
